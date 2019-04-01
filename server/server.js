01c0ac9556e2f257bff7f1f9e3f073c4af4ff988
Oleksandr Kovalov <kovalov.oleksandr@binary-studio.com>
2019-04-01 13:25:53 +0300

    add client part, use create react app for it. serve build folder as root of server app

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const path = require('path');
 const routes = require('./api/routes/index');
 const bodyParser = require('body-parser');
 
@@ -9,6 +10,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 routes(app);
+
+const staticPath = path.resolve(`${__dirname}/../client/build`);
+app.use(express.static(staticPath));
 
 app.listen(process.env.APP_PORT, () => {
     console.log(`Server listening on port ${process.env.APP_PORT}!`);
