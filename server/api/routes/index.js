19693e4602b850c5407791ccc522c5ead0828bbd
Alik <rakhmonov.alibek@binary-studio.com>
2019-04-10 13:00:17 +0300

    Remove route defenition.

--- server/api/routes/index.js
+++ server/api/routes/index.js
@@ -1,7 +1,5 @@
 import authRoutes from './auth.routes';
-import userRoutes from './user.routes';
 
 export default (app) => {
     app.use('/api/auth', authRoutes);
-    app.use('/api/users', userRoutes);
 };
