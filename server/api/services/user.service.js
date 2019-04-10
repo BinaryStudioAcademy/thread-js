02658d5ae1869574b74619503177e37312a2957c
Alik <rakhmonov.alibek@binary-studio.com>
2019-04-10 12:56:47 +0300

    Remove unused routes for now.

--- server/api/services/user.service.js
+++
@@ -1,6 +0,0 @@
-import userRepository from '../../data/repositories/user.repository';
-
-export default {
-    getAll: () => userRepository.getAll(),
-    getById: uid => userRepository.getById(uid)
-};
