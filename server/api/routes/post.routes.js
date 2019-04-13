94f16c78423e0944e463ac356e412f929107966c
AlikRakhmonov <alik.rahmonov@gmail.com>
2019-04-13 13:40:09 +0300

    Create post in server.

--- server/api/routes/post.routes.js
+++ server/api/routes/post.routes.js
@@ -9,6 +9,9 @@ router
         .catch(next))
     .get('/:id', (req, res, next) => postService.getPostById(req.params.id)
         .then(post => res.send(post))
+        .catch(next))
+    .post('/', (req, res, next) => postService.create(req.user.id, req.body)
+        .then(post => res.send(post))
         .catch(next));
 
 export default router;
--- server/api/services/post.service.js
+++ server/api/services/post.service.js
@@ -2,5 +2,9 @@ import postRepository from '../../data/repositories/post.repository';
 
 export default {
     getPosts: () => postRepository.getPosts(),
-    getPostById: id => postRepository.getPostById(id)
+    getPostById: id => postRepository.getPostById(id),
+    create: (userId, post) => postRepository.create({
+        ...post,
+        userId
+    })
 };
