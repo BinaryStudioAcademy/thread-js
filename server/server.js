471ab7a0e1553a85f3930fb4cd4c88e01ce9bbad
Oleksandr Kovalov <kovalov.oleksandr@binary-studio.com>
2019-04-19 16:18:47 +0300

    use server authorization middleware only for api routes. show client on all unmatched routes

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import routes from './api/routes/index';
 import authorizationMiddleware from './api/middlewares/authorization.middleware';
 import errorHandlerMiddleware from './api/middlewares/error-handler.middleware';
 import routesWhiteList from './config/routes-white-list.config';
+import fs from 'fs';
 
 import './config/passport.config';
 
@@ -15,15 +16,18 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
-app.use(authorizationMiddleware(routesWhiteList));
-
+app.get('/api/*', authorizationMiddleware(routesWhiteList));
 routes(app);
 
 const staticPath = path.resolve(`${__dirname}/../client/build`);
 app.use(express.static(staticPath));
 
-app.use(errorHandlerMiddleware);
+app.get('*', (req, res) => {
+    res.write(fs.readFileSync(`${__dirname}/../client/build/index.html`));
+    res.end();
+});
 
+app.use(errorHandlerMiddleware);
 app.listen(process.env.APP_PORT, () => {
     console.log(`Server listening on port ${process.env.APP_PORT}!`);
 });
