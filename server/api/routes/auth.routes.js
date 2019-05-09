ad2fe2007cb194d478ee0a07d2f3209fbfe3305d
Alik <rakhmonov.alibek@binary-studio.com>
2019-05-09 14:21:08 +0300

    return user when login/register. code refactoring

--- server/api/routes/auth.routes.js
+++ server/api/routes/auth.routes.js
@@ -1,15 +1,15 @@
 import { Router } from 'express';
-import authService from '../services/auth.service';
+import { login, register } from '../services/auth.service';
 import authenticationMiddleware from '../middlewares/authentication.middleware';
 import registrationMiddleware from '../middlewares/registration.middleware';
 
 const router = Router();
 
 router
-    .post('/login', authenticationMiddleware, (req, res, next) => authService.login(req.user)
+    .post('/login', authenticationMiddleware, (req, res, next) => login(req.user)
         .then(data => res.send(data))
         .catch(next))
-    .post('/register', registrationMiddleware, (req, res, next) => authService.register(req.user)
+    .post('/register', registrationMiddleware, (req, res, next) => register(req.user)
         .then(data => res.send(data))
         .catch(next));
 
--- server/api/services/auth.service.js
+++ server/api/services/auth.service.js
@@ -2,13 +2,15 @@ import tokenHelper from '../../helpers/token.helper';
 import cryptoHelper from '../../helpers/crypto.helper';
 import userRepository from '../../data/repositories/user.repository';
 
-export default {
-    login: async ({ id }) => tokenHelper.createToken({ id }),
-    register: async ({ password, ...user }) => {
-        const { id } = await userRepository.addUser({
-            ...user,
-            password: await cryptoHelper.encrypt(password)
-        });
-        return tokenHelper.createToken({ id });
-    }
+export const login = async ({ id, username, email }) => ({
+    token: tokenHelper.createToken({ id }),
+    user: { id, username, email }
+});
+
+export const register = async ({ password, ...userData }) => {
+    const newUser = await userRepository.addUser({
+        ...userData,
+        password: await cryptoHelper.encrypt(password)
+    });
+    return login(newUser);
 };
