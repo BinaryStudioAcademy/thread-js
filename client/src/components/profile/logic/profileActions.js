89052a15ebe11264e7774b160fe21884ff0cb487
Alik <rakhmonov.alibek@binary-studio.com>
2019-05-16 10:47:17 +0300

    code refactoring

--- client/src/components/profile/logic/profileActions.js
+++ client/src/components/profile/logic/profileActions.js
@@ -3,37 +3,33 @@ import { SET_USER, SET_IS_LOADING } from './profileActionTypes';
 
 const setToken = token => localStorage.setItem('token', token);
 
-export const setUser = user => async dispatch => dispatch({
+const setUser = user => async dispatch => dispatch({
     type: SET_USER,
     user
 });
 
-export const setIsLoading = isLoading => async dispatch => dispatch({
+const setIsLoading = isLoading => async dispatch => dispatch({
     type: SET_IS_LOADING,
     isLoading
 });
 
-const auth = authCallbackPromise => async (dispatch, getRootState) => {
-    setIsLoading(true)(dispatch, getRootState);
-    const { token, user } = await authCallbackPromise;
-    setToken(token);
+const setAuthData = (user = null, token = '') => (dispatch, getRootState) => {
     setUser(user)(dispatch, getRootState);
+    setToken(token);
+};
+
+const handleAuthResponse = authResponsePromise => async (dispatch, getRootState) => {
+    setIsLoading(true)(dispatch, getRootState);
+    const { user, token } = await authResponsePromise;
+    setAuthData(user, token)(dispatch, getRootState);
     setIsLoading(false)(dispatch, getRootState);
 };
 
-export const login = request => async (dispatch, getRootState) => {
-    auth(authService.login(request))(dispatch, getRootState);
-};
+export const login = request => handleAuthResponse(authService.login(request));
 
-export const registration = request => async (dispatch, getRootState) => {
-    auth(authService.registration(request))(dispatch, getRootState);
-};
+export const registration = request => handleAuthResponse(authService.registration(request));
 
-export const logout = () => (dispatch, getRootState) => {
-    setToken('');
-    setUser(null)(dispatch, getRootState);
-};
-
+export const logout = () => setAuthData();
 
 export const loadCurrentUser = () => async (dispatch, getRootState) => {
     setIsLoading(true)(dispatch, getRootState);
