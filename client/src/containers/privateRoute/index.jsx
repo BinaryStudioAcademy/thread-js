56c5f64f15716b9a891306065c48051640cedce2
Alik <rakhmonov.alibek@binary-studio.com>
2019-05-20 19:07:16 +0300

    refactoring component

--- client/src/containers/privateRoute/index.jsx
+++ client/src/containers/privateRoute/index.jsx
@@ -4,35 +4,20 @@ import { Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-class PrivateRoute extends React.Component {
-    renderComponent = (props) => {
-        const { Component, ...rest } = props;
-        return <Component {...rest} />;
-    }
-
-    render() {
-        const {
-            Component,
-            isAuthorized,
-            location,
-            ...rest
-        } = this.props;
-        return isAuthorized
-            ? <Route {...rest} render={this.renderComponent} />
-            : <Redirect to={{ pathname: '/login', state: { from: location } }} />;
-    }
-}
+const PrivateRoute = ({ isAuthorized, location, ...props }) => (
+    isAuthorized
+        ? <Route {...props} render={({ Component, ...rest }) => <Component {...rest} />} />
+        : <Redirect to={{ pathname: '/login', state: { from: location } }} />
+);
 
 PrivateRoute.propTypes = {
     isAuthorized: PropTypes.bool,
-    location: PropTypes.any,
-    Component: PropTypes.any
+    location: PropTypes.any
 };
 
 PrivateRoute.defaultProps = {
     isAuthorized: false,
     location: undefined,
-    Component: undefined
 };
 
 const mapStateToProps = rootState => ({
