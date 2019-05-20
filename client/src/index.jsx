286c6d4fab2c16546215712fbfbf1494747ec461
Alik <rakhmonov.alibek@binary-studio.com>
2019-05-20 19:02:06 +0300

    remove unused files

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,7 +4,7 @@ import Home from './scenes/Home';
 
 import './styles/reset.scss';
 import 'semantic-ui-css/semantic.min.css';
-import './styles/index.scss';
+import './styles/common.scss';
 
 const target = document.getElementById('root');
 render(<Home />, target);
