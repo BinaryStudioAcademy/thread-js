89d862e7701ce4d4faa14f6f3a773813a4f370df
what1s1ove <zubko.2007@gmail.com>
2021-02-18 21:29:15 +0200

    add-code-improvements: + js lint script

--- client/src/containers/Thread/index.jsx
+++ client/src/containers/Thread/index.jsx
@@ -1,5 +1,4 @@
-/* eslint-disable no-unused-vars */
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
