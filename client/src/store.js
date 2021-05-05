eb5e1498df845fefd613a2a7b2532070732c01cf
what1s1ove <zubko.2007@gmail.com>
2021-05-05 13:27:56 +0300

    reborn: + redux extension

--- client/src/store.js
+++ client/src/store.js
@@ -1,13 +1,8 @@
-import {
-  createStore,
-  applyMiddleware,
-  compose,
-  combineReducers
-} from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { connectRouter, routerMiddleware } from 'connected-react-router';
 import thunk from 'redux-thunk';
 import { createBrowserHistory } from 'history';
-// import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import threadReducer from './containers/Thread/reducer';
 import profileReducer from './containers/Profile/reducer';
@@ -16,14 +11,9 @@ export const history = createBrowserHistory();
 
 const initialState = {};
 
-const middlewares = [
-  thunk,
-  routerMiddleware(history)
-];
+const middlewares = [thunk, routerMiddleware(history)];
 
-const composedEnhancers = compose(
-  applyMiddleware(...middlewares)
-);
+const composedEnhancers = composeWithDevTools(applyMiddleware(...middlewares));
 
 const reducers = {
   posts: threadReducer,
@@ -35,10 +25,6 @@ const rootReducer = combineReducers({
   ...reducers
 });
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  composedEnhancers
-);
+const store = createStore(rootReducer, initialState, composedEnhancers);
 
 export default store;
