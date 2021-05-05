f438a7ee4d022d9d4f7fc4c30b43973bed60ff0c
what1s1ove <zubko.2007@gmail.com>
2021-05-05 15:59:29 +0300

    reborn: * spinner component

---
+++ client/src/components/common/index.js
@@ -0,0 +1,3 @@
+import Spinner from './spinner/spinner';
+
+export { Spinner };
---
+++ client/src/components/common/spinner/spinner.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Dimmer, Loader } from 'semantic-ui-react';
+
+const Spinner = () => (
+  <Dimmer active inverted>
+    <Loader size="massive" inverted />
+  </Dimmer>
+);
+
+export default Spinner;
