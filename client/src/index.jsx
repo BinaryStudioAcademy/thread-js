b27d0f6a50800a86caf07e409f37c9a4b3756c5d
what1s1ove <zubko.2007@gmail.com>
2021-05-05 14:53:01 +0300

    reborn: * root file

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import { render } from 'react-dom';
-import Home from './scenes/Home';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Routing from 'src/containers/Routing';
+import store from 'src/store/store';
 
 import './styles/reset.scss';
 import 'semantic-ui-css/semantic.min.css';
 import './styles/common.scss';
 
-const target = document.getElementById('root');
-render(<Home />, target);
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <Router>
+        <Routing />
+      </Router>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
