1706b47471ebd5a8736ef5095578f3e90514ad7f
what1s1ove <zubko.2007@gmail.com>
2021-05-05 13:12:43 +0300

    reborn: * private router component

--- client/src/containers/PrivateRoute/index.jsx
+++ client/src/containers/PrivateRoute/index.jsx
@@ -1,31 +1,35 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { locationType } from 'src/common/propTypes';
 
-const PrivateRoute = ({ component: Component, isAuthorized, ...rest }) => (
-  <Route
-    {...rest}
-    render={props => (isAuthorized
-      ? <Component {...props} />
-      : <Redirect to={{ pathname: '/login', state: { from: props.location } }} />)}
-  />
-);
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const { isAuthorized } = useSelector(state => ({
+    isAuthorized: state.profile.isAuthorized
+  }));
+
+  return (
+    <Route
+      {...rest}
+      render={props => (isAuthorized ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{ pathname: '/login', state: { from: props.location } }}
+        />
+      ))}
+    />
+  );
+};
 
 PrivateRoute.propTypes = {
-  isAuthorized: PropTypes.bool,
   component: PropTypes.elementType.isRequired,
   location: locationType
 };
 
 PrivateRoute.defaultProps = {
-  isAuthorized: false,
   location: undefined
 };
 
-const mapStateToProps = rootState => ({
-  isAuthorized: rootState.profile.isAuthorized
-});
-
-export default connect(mapStateToProps)(PrivateRoute);
+export default PrivateRoute;
