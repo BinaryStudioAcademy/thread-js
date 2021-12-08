7e13da3ab0ae0b04091e8b57680b303b9a337cee
Pavel Sukhinin <pavel.sukhinin@binary-studio.com>
2021-12-08 11:39:31 +0200

    redux-toolkit: - actions enums freeze

--- client/src/store/profile/common.js
+++ client/src/store/profile/common.js
@@ -1,7 +1,7 @@
-const ActionType = Object.freeze({
+const ActionType = {
   LOG_IN: 'profile/log-in',
   LOG_OUT: 'profile/log-out',
   REGISTER: 'profile/register'
-});
+};
 
 export { ActionType };
