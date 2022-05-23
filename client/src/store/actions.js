3b60368fab81f2a0f58f78c60599706be6fc123c
Vitalii Yarmus <vitalii.yarmys@binary-studio.com>
2022-05-23 11:45:14 +0300

    Fixed export of actions

--- client/src/store/actions.js
+++ client/src/store/actions.js
@@ -1,27 +1,2 @@
-import { login, register, logout, loadCurrentUser } from './profile/actions.js';
-import {
-  loadPosts,
-  loadMorePosts,
-  applyPost,
-  createPost,
-  toggleExpandedPost,
-  likePost,
-  addComment
-} from './thread/actions.js';
-
-export const profileActionCreator = {
-  login,
-  register,
-  logout,
-  loadCurrentUser
-};
-
-export const threadActionCreator = {
-  loadPosts,
-  loadMorePosts,
-  applyPost,
-  createPost,
-  toggleExpandedPost,
-  likePost,
-  addComment
-};
+export * as profileActionCreator from './profile/actions.js';
+export * as threadActionCreator from './thread/actions.js';
