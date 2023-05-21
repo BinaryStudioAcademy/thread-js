4052ee11c96f569a561a605ae941321ad746a57a
viacheslav-iskenderov <vyacheslav.iskenderov@binary-studio.com>
2023-05-21 15:35:18 +0300

    vi-refactor: back to destructurizing env && renamed tsconfigPaths to jsconfigPaths && vite config is a separate variable

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,25 +1,30 @@
 import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import reactPlugin from '@vitejs/plugin-react';
 // could be removed once vite aliases are done
-import tsconfigPaths from 'vite-jsconfig-paths';
+import jsconfigPaths from 'vite-jsconfig-paths';
 import dns from 'dns';
 
 // could be removed once node version is 17+
 dns.setDefaultResultOrder('verbatim');
 
-export default ({ mode }) => {
+const config = ({ mode }) => {
   // import.meta.env doesn't exist at this moment
-  const env = loadEnv(mode, process.cwd());
+  const { VITE_PORT, VITE_HOST, VITE_API_PATH, VITE_API_SERVER } = loadEnv(
+    mode,
+    process.cwd()
+  );
 
   return defineConfig({
     server: {
       // imgur forbids accessing its files from 127.0.0.1 (vite default host)
-      host: env.VITE_HOST,
-      port: Number(env.VITE_PORT),
+      host: VITE_HOST,
+      port: Number(VITE_PORT),
       proxy: {
-        [env.VITE_API_PATH]: env.VITE_API_SERVER
+        [VITE_API_PATH]: VITE_API_SERVER
       }
     },
-    plugins: [tsconfigPaths(), react()]
+    plugins: [jsconfigPaths(), reactPlugin()]
   });
 };
+
+export default config;
