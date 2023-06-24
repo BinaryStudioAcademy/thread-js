618ab3e3b8012d5b25648f8fcdc62502498a229f
Nickolay Grechkin <nickolaygrechkin@gmail.com>
2023-06-24 14:00:27 +0300

    thjs-51: * add VITE_SOCKET_PATH to .env.example

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ dns.setDefaultResultOrder('verbatim');
 
 const config = ({ mode }) => {
   // import.meta.env doesn't exist at this moment
-  const { VITE_PORT, VITE_HOST, VITE_API_PATH, VITE_API_SERVER, VITE_SOCKET_SERVER } = loadEnv(
+  const { VITE_PORT, VITE_HOST, VITE_API_PATH, VITE_API_SERVER, VITE_SOCKET_SERVER, VITE_SOCKET_PATH } = loadEnv(
     mode,
     process.cwd()
   );
@@ -25,7 +25,7 @@ const config = ({ mode }) => {
       port: Number(VITE_PORT),
       proxy: {
         [VITE_API_PATH]: VITE_API_SERVER,
-        '/socket.io': {
+        [VITE_SOCKET_PATH]: {
           target: VITE_SOCKET_SERVER,
           ws: true,
         },
