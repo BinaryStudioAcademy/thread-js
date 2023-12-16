b023aaa759ef1f9122a530bcc91a6c84f9131f00
Volodymyr Minchenko <voldemarprk2001@gmail.com>
2023-12-16 21:13:31 +0200

    thjs-63: * fix shared typescript build

--- shared/src/libs/helpers/date/get-from-now-time/get-from-now-time.helper.ts
+++ shared/src/libs/helpers/date/get-from-now-time/get-from-now-time.helper.ts
@@ -1,5 +1,5 @@
 import { dayjs } from '../dayjs/dayjs.js';
 
-const getFromNowTime = (date: Date): string => dayjs(date).fromNow();
+const getFromNowTime = (date: Date | string): string => dayjs(date).fromNow();
 
 export { getFromNowTime };
